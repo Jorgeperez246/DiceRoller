ab2f37b2fa4c8ffa011ef66d839949d7d21eb9e2
jorgeperez246 <jorgeperez246.jp@gmail.com>
2023-01-24 15:03:22 -0500

    added random number functionality when roll button is clicked

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton:Button=findViewById(R.id.roll_button)
-        rollButton.setOnClickListener { Toast.makeText(this,"button clicked",Toast.LENGTH_SHORT).show() }
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        val resultText : TextView = findViewById(R.id.result_text)
+        val randomInt = Random.nextInt(7)
+        resultText.text = randomInt.toString()
     }
 }
